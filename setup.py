ed2908009f8fc23b5fc553637c92d46ee95d76c8
Samuel Mueller <muellesa@tf.uni-freiburg.de>
2022-10-11 12:07:45 +0200

    make code packageable

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='tabpfn',
+      version='0.1',
+      description='Interface for using TabPFN and library to train TabPFN',
+      url='https://github.com/automl/TabPFN',
+      author='Noah Hollmann, Samuel Müller, Katharina Eggensperger, Frank Hutter',
+      author_email='muellesa@tf.uni-freiburg.de',
+      license='MIT',
+      packages=['tabpfn'],
+      python_requires='>=3.7',
+      zip_safe=False)
+
